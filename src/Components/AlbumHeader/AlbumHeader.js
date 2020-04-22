3606acd9909214c772465c02404f6853e9fc4be4
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-22 10:09:24 -0700

    Add AlbumHeader component
    Co-authored-by: Drake Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

---
+++ src/Components/AlbumHeader/AlbumHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <img src="https://miro.medium.com/max/2480/1*8FkvzbSdSJ4HNxtuZo5kLg.jpeg" />
+      <h1>Dark Side of the Moon - Pink Floyd</h1>
+    </React.Fragment>
+  )
+}
+
+export default Header;
