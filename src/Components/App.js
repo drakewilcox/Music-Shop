5c512d3bf66219e29925524f32e4976885aa5114
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-22 16:10:36 -0700

    Add and manage state in App.js with handlers
    Co-authored-by: Drake Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>
    Co-authored-by: jeremy kale padot <jeremy@padot.us>

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,51 +3,79 @@ import AlbumHeader from './AlbumHeader/AlbumHeader';
 import AlbumDetails from './AlbumDetails/AlbumDetails';
 import MainHeader from './MainHeader/MainHeader';
 import MainAlbum from './MainAlbums/MainAlbum';
+import '../App.css';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      showHomePage: true
+      showHomePage: true,
+      albumList: [
+        {
+          title: "Dark Side of the Moon",
+          artist: "Pink Floyd",
+          image: "https://miro.medium.com/max/2480/1*8FkvzbSdSJ4HNxtuZo5kLg.jpeg",
+          description: "An amazing album!",
+          id: "42066691"
+        },
+        {
+          title: "City To City",
+          artist: "Gerry Rafferty",
+          image: "https://upload.wikimedia.org/wikipedia/en/9/9a/Citytocity.jpg",
+          description: "An amazing album!",
+          id: "42066692"
+        },
+        {
+          title: "Blondes Have More Fun",
+          artist: "Rod Stewart",
+          image: "https://upload.wikimedia.org/wikipedia/en/3/35/Rod_Stewart_-_Blondes_Have_More_Fun_%28album_cover%29.jpg",
+          description: "An amazing album!",
+          id: "42066693"
+        },
+        {
+          title: "Africa",
+          artist: "TOTO",
+          image: "https://cdn-s3.allmusic.com/release-covers/500/0000/866/0000866482.jpg",
+          description: "An amazing album!",
+          id: "42066694"
+        },
+        {
+          title: "Can't Buy A Thrill",
+          artist: "Steely Dan",
+          image: "https://upload.wikimedia.org/wikipedia/en/b/b2/Cant_buy_a_tcant_buy_a_thrill.jpg",
+          description: "An amazing album!",
+          id: "42066695"
+        },
+        {
+          title: "Careless Whisper",
+          artist: "George Michael",
+          image: "https://img.discogs.com/j_OGjQLde9lJ7Om9Pd6gVHtuIpM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1296467-1354892016-3591.jpeg.jpg",
+          description: "An amazing album!",
+          id: "42066696"
+        }
+      ],
+      currentSelectedAlbum: {}
     }
   }
   
-  albumList = [
-    {
-      title: "Dark Side of the Moon",
-      artist: "Pink Floyd",
-      image: "https://miro.medium.com/max/2480/1*8FkvzbSdSJ4HNxtuZo5kLg.jpeg"
-    },
-    {
-      title: "City To City",
-      artist: "Gerry Rafferty",
-      image: "https://upload.wikimedia.org/wikipedia/en/9/9a/Citytocity.jpg"
-    },
-    {
-      title: "Blondes Have More Fun",
-      artist: "Rod Stewart",
-      image: "https://upload.wikimedia.org/wikipedia/en/3/35/Rod_Stewart_-_Blondes_Have_More_Fun_%28album_cover%29.jpg"
-    },
-    {
-      title: "Africa",
-      artist: "TOTO",
-      image: "https://cdn-s3.allmusic.com/release-covers/500/0000/866/0000866482.jpg"
-    },
-    {
-      title: "Can't Buy A Thrill",
-      artist: "Steely Dan",
-      image: "https://upload.wikimedia.org/wikipedia/en/b/b2/Cant_buy_a_tcant_buy_a_thrill.jpg"
-    },
-    {
-      title: "Careless Whisper",
-      artist: "George Michael",
-      image: "https://img.discogs.com/j_OGjQLde9lJ7Om9Pd6gVHtuIpM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1296467-1354892016-3591.jpeg.jpg"
-    }
-  ];
+  handleAlbumSelection = (id) => {
+    const selectedAlbum = this.state.albumList.filter(album => album.id === id)[0];
+    this.setState({
+      currentSelectedAlbum: selectedAlbum,
+      showHomePage: false
+    })
+  }
 
-  handleAlbumSelection = (event) => {
-    console.log(event.target.id);
+  handleAddingNewAlbum = (newAlbum) => {
+    const newAlbumList = this.state.albumList.concat(newAlbum);
+    this.setState({albumList: newAlbumList})
+  }
+
+  handleBackToAlbums = () => {
+    this.setState({
+      showHomePage: true
+    })
   }
 
   currentPage = () => {
@@ -55,13 +83,16 @@ class App extends React.Component {
       return {
         header: <MainHeader />,
         body: <MainAlbum 
-            albums={this.albumList}
-            handleAlbumSelection={this.handleAlbumSelection} />
+            albums={this.state.albumList}
+            onAlbumSelection={this.handleAlbumSelection}
+            onNewAlbumCreation={this.handleAddingNewAlbum} />
       }
     } else {
       return {
-        header: <AlbumHeader />,
-        body: <AlbumDetails />
+        header: <AlbumHeader 
+          album={this.state.currentSelectedAlbum}
+          handleBackToAlbums={this.handleBackToAlbums}/>,
+        body: <AlbumDetails album={this.state.currentSelectedAlbum}/>
       }
     }
   };
