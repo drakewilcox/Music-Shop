f4530835b757b419cfc12d6e2a17ce8f66712a61
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-22 10:08:33 -0700

    Add AlbumDetails component
    Co-authored-by: Drake Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

---
+++ src/Components/AlbumDetails/AlbumDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AlbumDetails = () => {
+  return (
+    <div>
+      <p>Description: dfdfsdfsdfsdfsdfds</p>
+      <div className="buttons">
+        <button>Buy</button>
+        <p>Quantity</p>
+        <button>Restock</button>
+      </div>
+    </div>
+  )
+}
+
+export default AlbumDetails;
