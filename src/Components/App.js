7cc2690f4ac687e20ce0f212c05c020edd73f343
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-23 12:22:40 -0700

    Update App.js with stocking methods and delete methods
    Co-authored-by: Drake Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>
    Co-authored-by: jeremy kale padot <jeremy@padot.us>

--- src/Components/App.js
+++ src/Components/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
           artist: "Pink Floyd",
           image: "https://miro.medium.com/max/2480/1*8FkvzbSdSJ4HNxtuZo5kLg.jpeg",
           description: "An amazing album!",
+          inventory: 9,
           id: "42066691"
         },
         {
@@ -24,6 +25,7 @@ class App extends React.Component {
           artist: "Gerry Rafferty",
           image: "https://upload.wikimedia.org/wikipedia/en/9/9a/Citytocity.jpg",
           description: "An amazing album!",
+          inventory: 9,
           id: "42066692"
         },
         {
@@ -31,6 +33,7 @@ class App extends React.Component {
           artist: "Rod Stewart",
           image: "https://upload.wikimedia.org/wikipedia/en/3/35/Rod_Stewart_-_Blondes_Have_More_Fun_%28album_cover%29.jpg",
           description: "An amazing album!",
+          inventory: 9,
           id: "42066693"
         },
         {
@@ -38,6 +41,7 @@ class App extends React.Component {
           artist: "TOTO",
           image: "https://cdn-s3.allmusic.com/release-covers/500/0000/866/0000866482.jpg",
           description: "An amazing album!",
+          inventory: 9,
           id: "42066694"
         },
         {
@@ -45,6 +49,7 @@ class App extends React.Component {
           artist: "Steely Dan",
           image: "https://upload.wikimedia.org/wikipedia/en/b/b2/Cant_buy_a_tcant_buy_a_thrill.jpg",
           description: "An amazing album!",
+          inventory: 9,
           id: "42066695"
         },
         {
@@ -52,6 +57,7 @@ class App extends React.Component {
           artist: "George Michael",
           image: "https://img.discogs.com/j_OGjQLde9lJ7Om9Pd6gVHtuIpM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1296467-1354892016-3591.jpeg.jpg",
           description: "An amazing album!",
+          inventory: 9,
           id: "42066696"
         }
       ],
@@ -78,6 +84,35 @@ class App extends React.Component {
     })
   }
 
+  handleAlbumPurchase = (id) => {
+    const selectedAlbum = this.state.albumList.filter(album => album.id === id)[0];
+    const newInventory = selectedAlbum.inventory -1;
+    const updatedAlbum = {...selectedAlbum, inventory: newInventory};
+    const oldAlbums = this.state.albumList.filter(album => album.id !== id);
+    this.setState({
+      albumList: [...oldAlbums, updatedAlbum],
+      currentSelectedAlbum: updatedAlbum
+    });
+  }
+  
+  handleAlbumRestock = (id) => {
+    const selectedAlbum = this.state.albumList.filter(album => album.id === id)[0];
+    const newInventory = selectedAlbum.inventory +1;
+    const updatedAlbum = {...selectedAlbum, inventory: newInventory}
+    const oldAlbums = this.state.albumList.filter(album => album.id !== id);
+    this.setState({
+      albumList: [...oldAlbums, updatedAlbum],
+      currentSelectedAlbum: updatedAlbum
+    });
+  }
+
+  handleAlbumDelete = id => {
+    const updateAlbumsList = this.state.albumList.filter(album => album.id !== id);
+    this.setState({
+      albumList: [...updateAlbumsList]
+    })
+  }
+
   currentPage = () => {
     if (this.state.showHomePage) {
       return {
@@ -85,14 +120,18 @@ class App extends React.Component {
         body: <MainAlbum 
             albums={this.state.albumList}
             onAlbumSelection={this.handleAlbumSelection}
-            onNewAlbumCreation={this.handleAddingNewAlbum} />
+            onNewAlbumCreation={this.handleAddingNewAlbum}
+            handleAlbumDelete={this.handleAlbumDelete} />
       }
     } else {
       return {
         header: <AlbumHeader 
           album={this.state.currentSelectedAlbum}
           handleBackToAlbums={this.handleBackToAlbums}/>,
-        body: <AlbumDetails album={this.state.currentSelectedAlbum}/>
+        body: <AlbumDetails 
+          album={this.state.currentSelectedAlbum}
+          onAlbumPurchase={this.handleAlbumPurchase}
+          onAlbumRestock={this.handleAlbumRestock}/>
       }
     }
   };
