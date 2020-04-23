a70480aa18ae1c817678b88f525be04917ad83d9
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-23 12:24:14 -0700

    Update MainHeader with dynamic state
    Co-authored-by: Drake Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>
    Co-authored-by: jeremy kale padot <jeremy@padot.us>

--- src/Components/MainHeader/MainHeader.js
+++ src/Components/MainHeader/MainHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MainHeader = (props) => {
   return (
     <div className="header">
-      <button type="button">🛒</button>
+      <button type="button">👄 </button>
       <h1>EPICODUS RECORDS</h1>
     </div>
   )
