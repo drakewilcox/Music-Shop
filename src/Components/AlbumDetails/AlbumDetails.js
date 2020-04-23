f02bcfa09f41d72c145e63d2fa372b8787d6d8d7
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-23 12:23:54 -0700

    Add dynamic state to AlbumDetails
    Co-authored-by: Drake Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>
    Co-authored-by: jeremy kale padot <jeremy@padot.us>

--- src/Components/AlbumDetails/AlbumDetails.js
+++ src/Components/AlbumDetails/AlbumDetails.js
@@ -2,20 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const AlbumDetails = (props) => {
+  
+  function hasInventory(count) {
+    return count > 0 ? <button onClick={() => props.onAlbumPurchase(props.album.id)}>Buy</button> : <p>Out Of Stock!</p>
+  }
+
   return (
-    <div >
+    <div className="description" >
       <p>Description: {props.album.description}</p>
+      <br />
       <div className="buttons">
-        <button>Buy</button>
-        <p>Quantity</p>
-        <button>Restock</button>
+        {hasInventory(props.album.inventory)}
+        <p>Quantity: {props.album.inventory}</p>
+        <button onClick={() => props.onAlbumRestock(props.album.id)}>Restock</button>
       </div>
     </div>
   )
 }
 
 AlbumDetails.propTypes = {
-  description: PropTypes.object
+  album: PropTypes.object,
+  onAlbumPurchase: PropTypes.func,
+  onAlbumRestock: PropTypes.func
 }
 
 export default AlbumDetails;
